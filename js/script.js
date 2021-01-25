5a37818290ec1e6eb2e135aa18e12e0e83157cf0
José Oliveira <jedgoliveira@gmail.com>
2021-01-25 23:42:18 +0000

    Introdução de JS

---
+++ js/script.js
@@ -0,0 +1,36 @@
+'use strict'
+
+var cloneMedia = $('.media').clone();
+var msg;
+
+$('#buttonSearch').on('click', function(){
+
+	var valorPesquisa = $('#pesquisa').val();
+	$('.panel-title').text('Resultados da Pesquisa para "' + valorPesquisa + '"');
+
+	$('.media-list').html('');
+
+	$.ajax({
+		method: "GET",
+		url: "http://www.last.fm/api/auth/?api_key=8ad8a891f9b7f2cefeb5ac37cd0487e9&token=2OH58H7cxviIJgbXC-El-aJNCf_63d8C" + valorPesquisa + "&r=json"
+	})
+
+
+
+
+	.done(function(msg){
+		console.log('msg');
+		console.log(msg);
+
+		msg.Search.forEach(function(result){
+			var liMedia = cloneMedia.clone();
+			$('#image', liMedia).attr("src", result.Poster);
+			$('.title', liMedia).text(result.Title);
+			$('.ano', liMedia).text(result.Year);
+			$('.tipo', liMedia).text(result.Type);
+
+			$('.media-list').append(liMedia);
+		})
+	})
+
+});
