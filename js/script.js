2d7a76df3bdb1c4c6ce7f3281985f3735bcaa895
José Oliveira <jedgoliveira@gmail.com>
2021-01-26 23:58:37 +0000

    Somethings

--- js/script.js
+++ js/script.js
@@ -1,36 +1,30 @@
-'use strict'
-
 var cloneMedia = $('.media').clone();
-var msg;
 
 $('#lupa').on('click', function(){
 
 	var valorPesquisa = $('#pesquisa').val();
-	$('.panel-title').text('Resultados da Pesquisa para "' + valorPesquisa + '"');
+	$('.panel-title').text('Resultados da pesquisa de "' + valorPesquisa + '"');
 
 	$('.media-list').html('');
 
 	$.ajax({
 		method: "GET",
-		url: "http://www.last.fm/api/auth/?api_key=8ad8a891f9b7f2cefeb5ac37cd0487e9&token=2OH58H7cxviIJgbXC-El-aJNCf_63d8C" + valorPesquisa + "&r=json"
+		url: "http://www.last.fm/api/auth/?api_key=8ad8a891f9b7f2cefeb5ac37cd0487e9&token=KidpygA4KK0PoiI75HU2GpHoAbl2D6gt" + valorPesquisa + "&r=json"
 	})
-
-
-
-
 	.done(function(msg){
-		console.log('msg');
+		//console.log('msg');
 		console.log(msg);
 
+		//$.each(msg.Search, function(index, result){
 		msg.Search.forEach(function(result){
 			var liMedia = cloneMedia.clone();
-			$('#image', liMedia).attr("src", result.Poster);
+			$('a', liMedia).attr('href', 'http://www.imdb.com/title/'+ result.imdbID);
+			if(result.Poster != 'N/A')
+				$('#image', liMedia).attr("src", result.Poster);
 			$('.title', liMedia).text(result.Title);
 			$('.ano', liMedia).text(result.Year);
 			$('.tipo', liMedia).text(result.Type);
-
 			$('.media-list').append(liMedia);
 		})
 	})
-
 });
