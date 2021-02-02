0483313f1a41204c187670bcedd466eecffceacc
davidteixeira <2201094@my.ipleiria.pt>
2021-02-02 00:02:39 +0000

    Mudanças implementação API

--- js/script.js
+++ js/script.js
@@ -9,19 +9,18 @@ $('#lupa').on('click', function(){
 
 	$.ajax({
 		method: "GET",
-		url: "http://www.last.fm/api/auth/?api_key=8ad8a891f9b7f2cefeb5ac37cd0487e9&token=KidpygA4KK0PoiI75HU2GpHoAbl2D6gt" + valorPesquisa + "&r=json"
+		url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + valorPesquisa + '&api_key=8ad8a891f9b7f2cefeb5ac37cd0487e9&format=json'
 	})
 	.done(function(msg){
-		//console.log('msg');
 		console.log(msg);
 
 		//$.each(msg.Search, function(index, result){
-		msg.Search.forEach(function(result){
+		msg.results.trackmatches.track.forEach(function(result){
 			var liMedia = cloneMedia.clone();
-			$('a', liMedia).attr('href', 'http://www.imdb.com/title/'+ result.imdbID);
+			$('a', liMedia).attr('href', result.url);
 			if(result.Poster != 'N/A')
 				$('#image', liMedia).attr("src", result.Poster);
-			$('.title', liMedia).text(result.Title);
+			$('.title', liMedia).text(result.name);
 			$('.ano', liMedia).text(result.Year);
 			$('.tipo', liMedia).text(result.Type);
 			$('.media-list').append(liMedia);
